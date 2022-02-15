8cb37e447b6387df1390a04d934842049f801beb
Guillaume C <81625005+Bledichef@users.noreply.github.com>
2022-02-15 14:08:56 +0100

    ok

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 
-const db_name = path.join(__dirname, "data", "apptest.db");
-const db = new sqlite3.Database(db_name, (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("Connexion réussie à la base de données 'apptest.db'");
-});
-next();
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
